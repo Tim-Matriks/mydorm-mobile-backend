caaed87cb19f3fc7d6e70458cdc428b7f38a2fed
Dias <78405123+diasdigital@users.noreply.github.com>
2025-01-04 06:48:04 +0700

    Perbaikan config

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-
-const db = new Sequelize(process.env.MYSQL_URL);
+const db = new Sequelize(process.env.MYSQL_URL, {
+    define: {
+        freezeTableName: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    },
+});
 
 module.exports = db;
