a55667cd1c8bc695e551303bb447ace9e52b068d
Dias <78405123+diasdigital@users.noreply.github.com>
2025-01-06 04:31:31 +0700

    Menambah multer awal

---
+++ src/middleware/multer.js
@@ -0,0 +1,36 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        const timestamp = new Date().getTime();
+
+        cb(null, `${timestamp}-${file.originalname}`);
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/png'
+    ) {
+        cb(null, true);
+    } else {
+        const errMsg = 'Hanya boleh upload gambar (jpeg, jpg, png)';
+        req.fileValidationError = errMsg;
+        return cb(new Error(errMsg), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 10 * 1000 * 1000,
+    },
+});
+
+module.exports = upload;
