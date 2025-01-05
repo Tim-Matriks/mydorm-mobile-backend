a54a62d3812215f7a45e3ef851283c7ee8c02758
Dias <78405123+diasdigital@users.noreply.github.com>
2025-01-06 02:27:10 +0700

    Menambah login helpdesk

---
+++ src/controllers/auth/helpdesk/LoginController.js
@@ -0,0 +1,64 @@
+const Helpdesk = require('../../../models/Helpdesk.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const handleLogin = async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ message: 'Username dan password wajib ada' });
+    }
+
+    try {
+        const helpdeskFound = await Helpdesk.findOne({
+            where: { username },
+        });
+        if (!helpdeskFound) {
+            return res
+                .status(401)
+                .json({ message: 'Username atau password salah' });
+        }
+        const verifikasi = await bcrypt.compare(
+            password,
+            helpdeskFound.password
+        );
+        if (!verifikasi) {
+            return res
+                .status(401)
+                .json({ message: 'Username atau password salah' });
+        }
+
+        const accessToken = jwt.sign(
+            { user_id: helpdeskFound.helpdesk_id, type: 'helpdesk' },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '60m' }
+        );
+        const refreshToken = jwt.sign(
+            { user_id: helpdeskFound.helpdesk_id, type: 'helpdesk' },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        await Helpdesk.update(
+            { refresh_token: refreshToken },
+            { where: { helpdesk_id: helpdeskFound.helpdesk_id } }
+        );
+
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000,
+        });
+        res.json({
+            message: 'Login berhasil',
+            user_type: 'Helpdesk',
+            accessToken,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Terjadi kesalahan pada server' });
+    }
+};
+
+module.exports = handleLogin;
---
+++ src/controllers/auth/helpdesk/RegisterController.js
@@ -0,0 +1,32 @@
+const Helpdesk = require('../../../models/Helpdesk.js');
+const { Op } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const handleRegister = async (req, res) => {
+    const { username, password, nip, nama } = req.body;
+    if (!username || !password || !nip || !nama) {
+        return res.status(400).json({ message: 'Data tidak lengkap' });
+    }
+
+    const duplicate = await Helpdesk.findOne({
+        where: { [Op.or]: [{ username }] },
+    });
+    if (duplicate)
+        return res.status(409).json({ message: 'Username sudah terdaftar' });
+
+    try {
+        const hashedPwd = await bcrypt.hash(password, 10);
+        const newHelpdesk = {
+            username,
+            password: hashedPwd,
+            nip,
+            nama,
+        };
+        await Helpdesk.create(newHelpdesk);
+        res.status(201).json({ message: 'User Helpdesk berhasil dibuat' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = handleRegister;
