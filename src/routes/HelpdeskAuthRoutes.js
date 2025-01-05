c16959e84426962ac92168b8ef2b2b943a15f5dc
Dias <78405123+diasdigital@users.noreply.github.com>
2025-01-06 05:03:55 +0700

    Perbaikan nama file

--- src/routes/HelpdeskAuthRoutes.js
+++ src/routes/HelpdeskAuthRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const registerController = require('../controllers/auth/helpdesk/RegisterController.js');
-const loginController = require('../controllers/auth/helpdesk/loginController.js');
-const logoutController = require('../controllers/auth/helpdesk/logoutController.js');
+const loginController = require('../controllers/auth/helpdesk/LoginController.js');
+const logoutController = require('../controllers/auth/helpdesk/LogoutController.js');
 
 router.post('/register', registerController);
 router.post('/login', loginController);
